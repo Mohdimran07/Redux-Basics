1d21599dd0ab865e6e5e446fbb2667ff681faf8d
Mohammed Imran <mohdimran97057@gmail.com>
2021-12-03 18:13:34 +0530

    first commit

---
+++ Redux-demo.js
@@ -0,0 +1,33 @@
+const redux = require('redux');
+
+const counterReducer = (state = { counter: 0 }, action) => {
+   if(action.type === 'increment'){
+    return {
+        counter: state.counter + 1,
+    };
+   }
+   if(action.type === 'decrement'){
+       return {
+           counter: state.counter - 1,
+       }
+   }
+  return state;
+};
+
+const store = redux.createStore(counterReducer);
+
+
+
+const counterSubscriber = () => {
+    const latestStore = store.getState();
+    console.log(latestStore);
+}
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: 'increment'});
+store.dispatch({ type: 'increment'});
+// store.dispatch({ type: 'increament'});
+// store.dispatch({ type: 'increament'});
+// store.dispatch({ type: 'increament'});
+store.dispatch({ type: 'decrement'});
